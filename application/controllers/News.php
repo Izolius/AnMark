57a5ee1b83d0108644f009cc5f537e49b3b9a062
MiKaMaru <obito_uchiha@inbox.ru>
2017-11-10 21:20:03 +0500

    Немного балуюсь с анимацией... https://www.w3schools.com/css/css3_buttons.asp

---
+++ application/controllers/News.php
@@ -0,0 +1,15 @@
+<?php
+include ("ANM_default_page.php");
+class Messages extends ANM_default_page {
+
+	public function index()
+	{
+		$header['css']=array("styles.css");
+		$title['logo']="logo_100_60";
+		$this->load->view('header', $header);
+		$this->load->view('controls/Title',$title);
+		$this->load->view('controls/left_menu');
+		$this->load->view('footer');
+	}
+}
+?>
