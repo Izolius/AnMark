e9f76dd50daa784265be8eb18f5725667c670d57
MiKaMaru <obito_uchiha@inbox.ru>
2017-11-10 09:54:35 +0500

    little disign fix

--- application/views/controls/left_menu.php
+++ application/views/controls/left_menu.php
@@ -1,5 +1,6 @@
 <div id="left_menu" class="left_menu">
 <?php $this->view('controls/left_menu_button',array('site'=>'main', 'text'=>'Главная'));?>
 <?php $this->view('controls/left_menu_button',array('site'=>'friends', 'text'=>'Друзья'));?>
-<?php $this->view('controls/left_menu_button',array('site'=>'messages', 'text'=>'Сообщения'));?>      
+<?php $this->view('controls/left_menu_button',array('site'=>'messages', 'text'=>'Сообщения'));?>
+<?php $this->view('controls/left_menu_button',array('site'=>'news', 'text'=>'Новости'));?>          
 </div>
