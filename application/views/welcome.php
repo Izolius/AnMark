b3e63963aa1f62b3f8b1272b7f72be6510882822
MiKaMaru <obito_uchiha@inbox.ru>
2017-12-05 04:08:52 +0500

    Небольшой фикс

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,20 +1,20 @@
 
 <?php echo form_open('welcome'); ?>
 <div>
-
+<?php echo form_error('email'); ?>
 <div class="bordered login_form_text " >
 <div class="field_name">
             <p><span><?php echo ('Адрес электронной почты');?></span></p>
             </div>
-<?php echo form_error('email'); ?>
+
 <p><span><input type="text" name="email" value="<?php echo set_value('email'); ?>" size="94" /></span></p>
 </div>
-
+<?php echo form_error('password'); ?>
 <div class="bordered login_form_text " >
 <div class="field_name">
             <p><span><?php echo ('Пароль');?></span></p>
             </div>
-<?php echo form_error('password'); ?>
+
 <p><span><input type="password" name="password" value="<?php echo set_value('password'); ?>" size="94" /></span></p>
 </div>
 
